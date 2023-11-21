5757cd2b9d3a65c417b4bc12e7558a19e9a0f2cd
Philipp Buling <buling.philipp@gmail.com>
2023-11-21 20:58:26 +0100

    Update

--- jahrgang-2022/js/erfassung.js
+++ jahrgang-2022/js/erfassung.js
@@ -1,3 +1,21 @@
+const form = document.getElementById('fahrtenbuchForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Verhindert das Standardverhalten des Formulars
+
+        // Lese die eingegebenen Werte aus den Eingabefeldern
+        const datum = document.getElementById('datum').value;
+        const kilometerstand = document.getElementById('kilometerstand').value;
+        const grund = document.getElementById('grund').value;
+
+        // Rufe die Funktion addFahrt auf und übergebe die Werte
+        addFahrt(datum, kilometerstand, grund);
+
+        // Setze die Eingabefelder zurück
+        form.reset();
+        window.location.href = "verwaltung.html";
+    });
+
 function addFahrt(datum, kilometerstand, grund) {
     let fahrt = {
         datum: new Date(datum), // Datum als Date-Objekt speichern
